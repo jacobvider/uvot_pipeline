350a840c572d12bf0e161971b81a644d27953339
Jacob Vider <60020199+jacobvider@users.noreply.github.com>
2026-08-24 15:10:41 -0400

    Update main.py to import process from processing

--- main.py
+++ main.py
@@ -18,16 +18,15 @@
 #     process(observation_path)
 #     print("Done.")
 
-
 # if __name__ == "__main__":
 #     main("sw00083650004uuu_sk.img.gz")
 
 from pathlib import Path
 
-from uvot_pipeline.processing import process
-# from processing import process
+print("In main.py: import process")
+from processing import process
 
-
+print("In main.py: define path")
 DATA_DIR = Path(
     "/mnt/c/Users/jacob/research/spring_2026/"
     "image_subtraction_pipeline_to_process"
@@ -37,13 +36,22 @@ def main(filename):
     """
     Process a single Swift UVOT observation.
     """
+    print("In main.py: set observation_path")
 
     observation_path = DATA_DIR / filename
+    print(observation_path)
+
+    print("In main.py: check if observation_path exists")
 
     if not observation_path.exists():
+        print("In main.py: path does not exist")
+
         raise FileNotFoundError(observation_path)
+    
+
 
     print(f"Processing {observation_path.name}")
+    print("In main.py: run process from processing.py")
 
     result = process(observation_path)
 
@@ -53,4 +61,4 @@ def main(filename):
 
 
 if __name__ == "__main__":
-    main("sw00083650004uuu_sk.img.gz")
+    main("sw00030009002uw1_sk.img.gz")
